419c3490382e360bc26dff87f54a069ca616e68f
deshi9012 <ilia.bojadzhiev@gmail.com>
2021-01-17 18:54:34 +0200

    dir changes

---
+++ mobile/screens/TimelineScreen.js
@@ -0,0 +1,84 @@
+import React, {useState,useRef, useEffect} from 'react';
+import {
+  View,
+  Button,
+  FlatList,
+  StatusBar,
+  StyleSheet,
+  Text
+} from 'react-native';
+import axios from 'axios';
+import Item from "../components/Item";
+import Constants from 'expo-constants';
+import * as Notifications from 'expo-notifications';
+import * as firebase from "firebase";
+
+const TimelineScreen = props => {
+  const URI = 'http://localhost:8000';
+
+  const [items, setItems] = useState([]);
+  const fetchItems = async () => {
+    try {
+      const response = await axios.get(URI + '/api/items');
+      setItems(response.data);
+    } catch (err){
+      console.log(err);
+    }
+  }
+
+  const navigateItemHandler = (itemId) => {
+    props.navigation.navigate({
+      routeName: 'ItemDetails', params: {
+        itemId: itemId
+      }
+    })
+  };
+
+  useEffect(() => {
+    fetchItems();
+  }, []);
+
+  return (
+    <View style={styles.screen}>
+      <Text>The Timeline Screen!</Text>
+      <FlatList
+        keyExtractor={(item,index) => index.toString()}
+        data={items}
+        renderItem={itemData => (
+          <Item
+            id={itemData.item.id}
+            title={itemData.item.title}
+            onPress={navigateItemHandler}
+          />
+        )}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  screen: {
+    padding: 50
+  },
+  inputContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center'
+  },
+  input: {
+    width: '80%',
+    borderColor: 'black',
+    borderWidth: 1,
+    padding: 10
+  },
+  listItem: {
+    padding: 10,
+    marginVertical: 10,
+    backgroundColor: '#ccc',
+    borderColor: 'black',
+    borderWidth: 1
+  }
+});
+
+
+export default TimelineScreen;
